6a2236440dc43bb0ab774b0bfbbf457fec199494
tommyno <tommyno@users.noreply.github.com>
2015-10-08 09:37:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,20 +8,21 @@
 
 })();
 
-
+/*
 // jQuery
 $(document).ready(function() {
 
 	// do something
 
 }); 
+*/
 
 
-
-
+/*
 // Attach Fastclick
 if ('addEventListener' in document) {
     document.addEventListener('DOMContentLoaded', function() {
         FastClick.attach(document.body);
     }, false);
 }
+*/
